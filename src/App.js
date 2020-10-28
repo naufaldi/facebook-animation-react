35394012fa2f700448716e42b427a7b6d189d99b
Naufaldi Rafif <naufaldirafif@Faiks-MacBook-Air.local>
2020-10-28 09:18:37 +0700

    add transition grup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar>
+      <NavItems icon="🛵" />
+      <NavItems icon="🛵" />
+      <NavItems icon="🛵" />
+      <NavItems icon="🛵" >
+        <DropdownMenu />
+      </NavItems>
+
+    </Navbar>
   );
+}
+
+function Navbar(props) {
+  return (
+    <navbar className="navbar">
+      <ul className="navbar-nav">
+        {props.children}
+      </ul>
+    </navbar>
+  )
+}
+function NavItems(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => { setOpen(!open) }}>
+        {props.icon}
+      </a>
+      { open && props.children}
+    </li>
+  )
+}
+
+function DropdownMenu() {
+  function DropdownItem(props) {
+    return (
+      <a href="" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-button">{props.rightIcon}</span>
+      </a>
+    )
+  }
+  return (
+    <div className="dropdown">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem
+        leftIcon={'😂'}
+        rightIcon={'😍'}
+      >
+
+      </DropdownItem>
+    </div>
+  )
 }
 
 export default App;
